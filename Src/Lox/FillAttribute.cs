bde2087cb99f32bc3f02339188b9b0aafb5c74c7
FaberSanZ <faber.reach@gmail.com>
2025-07-28 14:18:58 -0500

    Update

--- Src/Lox/FillAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Lox
-{
-    internal class FillAttribute : Attribute
-    {
-    }
-}
--- Src/Lox/Syntax/ErrorType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lox
-{
-    enum ErrorType
-    {
-        SyntaxError,
-        SemanticError
-    }
-}
